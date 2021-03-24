9cc72196d09ce69bf55630a71eeb46f75f92c897
vengateshl <62301944+vengateshl@users.noreply.github.com>
2021-03-24 12:32:35 +0530

    Add files via upload

---
+++ src/main/java/com/jar/App.java
@@ -0,0 +1,13 @@
+package com.jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello this is vengat!" );
+    }
+}
